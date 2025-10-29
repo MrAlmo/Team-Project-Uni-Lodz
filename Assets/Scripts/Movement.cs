8f908c3649c9e0367d5adf43aca5d6a509fe8685
MrAlmo <tutrest5003@gmail.com>
2025-10-29 16:14:23 +0100

    fix movement v1.0

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody2D m_Rigidbody2D;
-    [SerializeField]float speed = 1.0f;
-    [SerializeField]float jump = 10.0f;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jump = 10f;
+    [SerializeField] Transform GroundCheck;
+    [SerializeField] float groundCheckRadius = 0.2f;
+    [SerializeField] LayerMask groundlayer;
+    
     float move_Raw;
-       
+    bool is_Grounded = false;   
     
     void Start()
     {
@@ -20,18 +24,27 @@ public class Movement : MonoBehaviour
     {
         move_Raw = Input.GetAxisRaw("Horizontal");
 
+        is_Grounded = Physics2D.OverlapCircle(GroundCheck.position, groundCheckRadius, groundlayer);
         
-        //m_Rigidbody2D.AddForce(Vector2.right * Input.GetAxisRaw("Horizontal") * speed);
-
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && is_Grounded)
         {
-            m_Rigidbody2D.AddForce(Vector2.up * jump);
+            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, jump);
         }
     }
 
     private void FixedUpdate()
     {
-        Vector2 targetposition = m_Rigidbody2D.position + Vector2.right * move_Raw * speed * Time.fixedDeltaTime;
-        m_Rigidbody2D.MovePosition(targetposition);
+        //Vector2 targetposition = m_Rigidbody2D.position + Vector2.right * move_Raw * speed * Time.fixedDeltaTime;
+        //m_Rigidbody2D.MovePosition(targetposition);
+
+        m_Rigidbody2D.velocity = new Vector2(move_Raw * speed, m_Rigidbody2D.velocity.y);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (GroundCheck != null)
+        {
+            Gizmos.DrawWireSphere(GroundCheck.position, groundCheckRadius);
+        }   
     }
 }
