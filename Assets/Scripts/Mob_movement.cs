54b47a60e428b6f2e52b42c0a25f738a8109e938
Andriicsfv <mrmastet522@gmail.com>
2025-11-26 19:47:12 +0100

    Dialogue/Npc sprite and animation/Partly Main menu

--- Assets/Scripts/Mob_movement.cs
+++ Assets/Scripts/Mob_movement.cs
@@ -2,32 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mob_movement : MonoBehaviour
-{
-    [SerializeField]private float speed = 5f;
-    //[SerializeField]private float jumpforce = 3f;
-    [SerializeField]private float switchtime = 2f;
 
+
+public class Mob_movement : MonoBehaviour
+
+{
+
+     [SerializeField] private float speed = 5f;
+
+    
+
+    [SerializeField] private float switchtime = 2f;
     Rigidbody2D rb;
     private float timer;
     private int direction = 1;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+   void Start()
 
-    
-    void Update()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+       }
+     void Update()
+
     {
         timer += Time.deltaTime;
+       if (timer >= switchtime)
 
-        if (timer >= switchtime)
         {
+
             direction *= -1;
+
             timer = 0f;
+
         }
 
+
+
         rb.velocity = new Vector2(direction * speed, rb.velocity.y);
-        
-    }
+
+         }
+
 }
--- Assets/Scripts/Mob_movement_strict.cs
+++ Assets/Scripts/Mob_movement_strict.cs
@@ -1,32 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Mob_movement_strict : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    //[SerializeField] private float jumpforce;
     [SerializeField] Transform[] points;
 
     private int current = 0;
     Rigidbody2D rb;
-    
+    Animator m_Animator; 
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>(); 
     }
 
-    
     void Update()
     {
         if (points.Length == 0) return;
 
         Vector2 target = points[current].position;
+        
         Vector2 direction = (target - (Vector2)transform.position).normalized;
 
         rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
 
+        if (m_Animator != null)
+        {
+            
+            
+            m_Animator.SetFloat("MoveX", direction.x);
+
+            
+            bool isMoving = Mathf.Abs(rb.velocity.x) > 0.1f;
+            m_Animator.SetBool("Movement", isMoving);
+        }
+       
+
+        
         if (Mathf.Abs(target.x - transform.position.x) < 0.1f)
         {
             current += 1;
